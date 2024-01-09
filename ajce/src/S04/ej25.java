f1fbe4decc4f933a4b012335b44389e9b1235744
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-01-09 11:05:00 -0300

    100 dias de java dia 18 30/12/23 S04 ej21 al 25

--- ajce/src/S04/ej25.java
+++ ajce/src/S04/ej25.java
@@ -1,9 +1,21 @@
 package S04;
 
+import java.util.Scanner;
+
 public class ej25 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char res;
+						
+		do {
+			
+			
+			System.out.print("¿Desea continuar? (S/N): "); res = sc.next().charAt(0);
+			sc.nextLine();
+		} while (Character.toUpperCase(res) == 'S');
+		
+		sc.close();
 
 	}
 
