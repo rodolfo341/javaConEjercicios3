d375bb689461c53253b7391306169ffc43e9de59
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-01-13 21:32:22 -0300

    100 dias de java dia 20 13/01/24 S04 ej24 al ej29 ultimos

--- ajce/src/S04/ej29.java
+++ ajce/src/S04/ej29.java
@@ -1,7 +1,19 @@
 package S04;
 
 import java.util.Scanner;
-
+/**
+ * Realiza un programa que calcule el precio de un desayuno. 
+ * El programa preguntará primero 
+ * qué ha tomado el usuario de comer: 
+ * 			.- palmera ( 1.40 € )
+ * 			.- donut ( 1 € )
+ * 			.- pitufo
+ * 				.- con aceite ( 1’20 € )
+ * 				.- con tortilla ( 1’60 € )
+ * la bebida: 
+ * 			.- zumo ( 1’50 )
+ * 			.- café ( 1’20 )
+ */
 public class ej29 {
 
 	public static void main(String[] args) {
